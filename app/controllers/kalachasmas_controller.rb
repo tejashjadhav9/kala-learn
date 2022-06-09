ecf79edd5232c65576c7b7ff63d4c239078d5d71
tejashjadhav9 <ja.tejash@gmail.com>
2022-06-10 00:09:45 +0530

    first commit

---
+++ app/controllers/kalachasmas_controller.rb
@@ -0,0 +1,2 @@
+class KalachasmasController < ApplicationController
+end
---
+++ db/migrate/20220521061502_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.integer :age
+      t.bigint :phone
+      t.text :email
+
+
+      t.timestamps
+    end
+  end
+end
